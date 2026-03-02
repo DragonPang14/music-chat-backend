c5039eee6cb5a381a81e759bccdb435d3410f232
dragon <GLDZPang@outlook.com>
2026-03-02 22:08:15 +0800

    feat: init project

---
+++ src/main/java/com/electrocardiogram/musicchatbackend/MusicChatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.electrocardiogram.musicchatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicChatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicChatBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/electrocardiogram/musicchatbackend/MusicChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electrocardiogram.musicchatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicChatBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
